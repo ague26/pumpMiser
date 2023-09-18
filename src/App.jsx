406279018f553d73aa4b096e2ebb40d9e8522c87
Alexander Guevara <alexgue26@hotmail.com>
2023-09-18 16:52:19 -0400

    Added appliance icons and created control panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
 import BG from "./assets/bg-pump-miser.png"
+import Bath from "./assets/Bath-Tub.png"
+import Dishwasher from "./assets/Dishwasher.png"
+import Laundry from "./assets/Laundry-Machine.png"
+import Sprinkler from "./assets/Sprinkler.png"
+import Toilet from "./assets/Toilet.png"
+import Kitchen from "./assets/Washing-Dish.png"
+
 function App() {
 
   return (
     <>
       {/* <div className="bg-auto bg-no-repeat" style={{backgroundImage:`url(${BG})`}}> */}
-      <div className="relative">  
+      <div className="relative max-w-7xl m-auto">  
         <img src={BG} className="bg-contain" />
-        <div id="control-panel" className="flex justify-center absolute items-center top-0">
-          <div id="water-usage-box" className="border-2 m-2 p-2">
-            <div id="kitchen">
-              <img></img>
-              <p>RUN KITCHEN SINK</p>
+        <div id="control-panel" className="flex justify-center absolute items-center top-1/4 right-3">
+          <div id="water-usage-box" className="border-2 m-3 p-2">
+            <div id="kitchen" className="house-service">
+              <div className="appliance-icon">
+                <img src={Kitchen}/>
+              </div>
+              <p className="text-xs">RUN KITCHEN SINK</p>
             </div>
-            <div id="dishwasher">
-              <img></img>
-              <p>RUN DISHWASHER</p>
+            <div id="dishwasher" className="house-service">
+              <div className="appliance-icon">
+                <img src={Dishwasher}/>
+              </div>
+              <p className="text-xs">RUN DISHWASHER</p>
             </div>
-            <div id="laundry">
-              <img></img>
-              <p>WASH LAUNDRY</p>
+            <div id="laundry" className="house-service">
+              <div className="appliance-icon">
+                <img src={Laundry}/>
+              </div>
+              <p className="text-xs">WASH LAUNDRY</p>
             </div>
-            <div id="shower">
-              <img></img>
-              <p>TAKE A SHOWER</p>
+            <div id="shower" className="house-service">
+              <div className="appliance-icon">
+                <img src={Bath}/>
+              </div>
+              <p className="text-xs">TAKE A SHOWER</p>
             </div>
-            <div id="sprinklers">
-              <img></img>
-              <p>SPRINKLERS</p>
+            <div id="sprinklers" className="house-service">
+              <div className="appliance-icon">
+                <img src={Sprinkler}/>
+              </div>
+              <p className="text-xs">SPRINKLERS</p>
             </div>
-            <div id="toilet">
-              <img></img>
-              <p>FLUSH TOILET</p>
+            <div id="toilet" className="house-service">
+              <div className="appliance-icon">
+                <img src={Toilet}/>
+              </div>
+              <p className="text-xs">FLUSH TOILET</p>
             </div>
           </div>
 
         </div>
       </div>
+      
+      
       <div>
 
       </div>
